004b50f6ea9003acc487d87517dde60c3b554f57
Saran Tanpituckpong <sarunta@gmail.com>
2020-10-23 15:17:24 +0700

    ✨ Add discord command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -226,3 +226,17 @@ Function npp {
 		& $NPPPath $Path
 	}
 }
+
+<#
+.SYNOPSIS
+	Rerun Discord as administrator.
+#>
+function discord {
+	$discordUpdateExePath = Join-Path $env:LOCALAPPDATA 'Discord/Update.exe'
+	$discordArgs = @('--processStart Discord.exe')
+
+	if ([bool] (Get-Process -Name 'Discord' -ErrorAction SilentlyContinue)) {
+		Stop-Process -Name 'Discord' -Force
+	}
+	Start-Process $discordUpdateExePath -ArgumentList $discordArgs -Verb RunAs
+}
