d637785b0e3809e5441a887a75683fccfe385c17
Saran Tanpituckpong <sarunta@gmail.com>
2025-09-10 00:10:37 +0700

    ✨ Add dotnet completion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -86,6 +86,15 @@ if (Test-CommandExists eza) {
 	}
 }
 
+# dotnet
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+	param($wordToComplete, $commandAst, $cursorPosition)
+	dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	}
+}
+
 # Functions
 <#
 .SYNOPSIS
